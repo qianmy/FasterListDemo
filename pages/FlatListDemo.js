a7edc241d707b13644364a95f2da1db250d919c4
v_qianmaoyin <562510392@qq.com>
2018-10-25 11:31:01 +0800

    FlatList的下拉刷新和上拉加载

--- pages/FlatListDemo.js
+++ pages/FlatListDemo.js
@@ -7,11 +7,19 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, FlatList} from 'react-native';
+import {Platform, StyleSheet, Text, View, FlatList, RefreshControl, ActivityIndicator} from 'react-native';
 
 type Props = {};
-const CITY_NAMES = ['北京', '上海', '广州', '深圳', '杭州', '苏州', '北京', '上海', '广州', '深圳', '杭州', '苏州'];
+const CITY_NAMES = ['北京', '上海', '广州', '深圳', '杭州', '苏州'];
 export default class FlatListDemo extends Component<Props> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoading: false,
+            dataArray: CITY_NAMES,
+        }
+    }
+
     _renderItem(data) {
         return (
             <View style={styles.item}>
@@ -20,12 +28,70 @@ export default class FlatListDemo extends Component<Props> {
         )
     }
 
+    //refreshing为true时，“下拉刷新”的动作触发这个方法；否则则是“上拉加载”
+    loadData(refreshing) {
+        if (refreshing) {
+            this.setState({
+                isLoading: true,//显示正在加载的符号
+            });
+        }
+
+        //仿照网络请求一个新的数据源
+        setTimeout(() => {
+            let dataArray = [];
+            if (refreshing) {//“下拉刷新”
+                dataArray = this.state.dataArray.reverse();
+            } else {//“上拉加载”
+                dataArray = this.state.dataArray.concat(CITY_NAMES);
+            }
+
+            this.setState({
+                dataArray: dataArray,//修改数据源
+                isLoading: false,//隐藏正在加载的符号
+            })
+        }, 5000)
+    }
+
+    genIndicator = () => {//“上拉加载”的动作触发这个方法
+        return (
+            <View style={styles.indicatorContainer}>
+                <ActivityIndicator
+                    style={styles.indicator}
+                    size={'large'}
+                    color={'red'}
+                    animating={true}
+                />
+                <Text>正在加载更多</Text>
+            </View>
+        )
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <FlatList
-                    data={CITY_NAMES}
-                    renderItem={(data)=>this._renderItem(data)}
+                    data={this.state.dataArray}
+                    renderItem={(data) => this._renderItem(data)}
+                    // refreshing={this.state.isLoading}
+                    // onRefresh={()=>{
+                    //     this.loadData()
+                    // }}
+                    refreshControl={
+                        <RefreshControl
+                            title={'Loading'}
+                            colors={['blue']}//android特有
+                            tintColor={'orange'}//ios特有
+                            titleColor={'red'}//ios特有
+                            refreshing={this.state.isLoading}
+                            onRefresh={() => {
+                                this.loadData(true)
+                            }}
+                        />
+                    }
+                    ListFooterComponent={this.genIndicator}
+                    onEndReached={() => {
+                        this.loadData()
+                    }}
                 />
             </View>
         );
@@ -41,13 +107,19 @@ const styles = StyleSheet.create({
         height: 200,
         marginRight: 15,
         marginLeft: 15,
-        marginBottom:15,
+        marginBottom: 15,
         alignItems: 'center',
         justifyContent: 'center'
     },
     text: {
         color: 'white',
         fontSize: 20
+    },
+    indicatorContainer: {
+        alignItems: 'center'
+    },
+    indicator: {
+        color: 'red',//这个设置不起作用，在组件ActivityIndicator中直接设置的color起作用
+        margin: 10
     }
-
 });
